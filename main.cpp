eea0e9e364411934f8a36d95daceec50d07ab8eb
12311249 <lgonzalezs14@miumg.edu.gt>
2025-03-08 10:34:40 -0600

    Ejercicio 2.1 realizado por Luis Diego Gonzalez Sanchez 0909-22-1214

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num =0;
+    while (num <= 33) {
+        if (num % 3 == 0 && num % 3 == 0) {
+            cout << num << endl;
+        }
+        num++;
+    }
+
+
+    return 0;
+}
